50266986f08fb72132e15a1981b36b34f4c9b789
slainer68 <slainer68@gmail.com>
2014-05-22 12:37:55 +0200

    Send 404 instead of 500

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require 'dragonfly'
 
 app = Dragonfly.app(:images).configure_with(:imagemagick)
 
-get '/hi' do
-  "Hello World!"
+get '/images/:size.:format' do |size, format|
+  format_response(app.fetch_url(Rack::Utils.unescape(params[:url])).thumb(size).encode(format).to_response(env))
 end
 
-get '/images/:size.:format' do |size, format|
-  app.fetch_url(Rack::Utils.unescape(params[:url])).thumb(size).encode(format).to_response(env)
+private
+def format_response(res)
+  if res && res.is_a?(Array) && res[0] == 500
+    [404, {"Content-Type"=>"text/plain"}, ["Not found"]]
+  else
+    res
+  end
 end
